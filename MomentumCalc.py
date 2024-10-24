ef5947240fe12e72134d8fe0f6942c3b14a28ce0
Ally <67673392+Allybe@users.noreply.github.com>
2024-10-24 12:31:53 -0500

    init commit

---
+++ MomentumCalc.py
@@ -0,0 +1,20 @@
+print("Momentum Calculator by Allison Miranda")
+
+user_input = int(input("Number of objects in system: "))
+
+if user_input == 0:
+    print("Momentum: 0")
+
+totalInitialMomentum = 0
+
+for x in range(user_input):
+    mass = int(input("Enter Mass (Kg): "))
+    velocity = int(input("Enter Velocity (m/s): "))
+
+    totalMomentum = mass * velocity
+    # clear screen
+
+print("Momentum: " + str(totalMomentum))
+
+
+
